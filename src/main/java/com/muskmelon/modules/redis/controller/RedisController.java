8f3bba47f736f2803222f2504d5ca4bcc5d9f564
xianggua <1013830051@qq.com>
2020-02-11 00:10:45 +0800

    [ADD] 增加redis操作日志注解

--- src/main/java/com/muskmelon/modules/redis/controller/RedisController.java
+++ src/main/java/com/muskmelon/modules/redis/controller/RedisController.java
@@ -1,6 +1,7 @@
 package com.muskmelon.modules.redis.controller;
 
 import com.muskmelon.common.action.ActionResult;
+import com.muskmelon.common.annotation.LoggerOperator;
 import com.muskmelon.modules.redis.service.RedisService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ public class RedisController {
     @Resource
     private RedisService redisService;
 
+    @LoggerOperator(description = "连接redis服务")
     @RequestMapping(value = "/connect", method = RequestMethod.POST)
     public ActionResult<String> connect(String ip, int port, String password) {
         ActionResult<String> result = new ActionResult<>();
@@ -28,6 +30,7 @@ public class RedisController {
         return result;
     }
 
+    @LoggerOperator(description = "关闭redis服务")
     @RequestMapping(value = "/close", method = RequestMethod.POST)
     public ActionResult<String> connect() {
         ActionResult<String> result = new ActionResult<>();
@@ -35,6 +38,7 @@ public class RedisController {
         return result;
     }
 
+    @LoggerOperator(description = "获取缓存")
     @RequestMapping(value = "/get", method = RequestMethod.POST)
     public ActionResult<String> getKeyValue(String key) {
         ActionResult<String> result = new ActionResult<>();
@@ -42,6 +46,7 @@ public class RedisController {
         return result;
     }
 
+    @LoggerOperator(description = "设置缓存")
     @RequestMapping(value = "/set", method = RequestMethod.POST)
     public ActionResult<String> setKeyValue(String key, String value, int expire) {
         ActionResult<String> result = new ActionResult<>();
@@ -49,6 +54,7 @@ public class RedisController {
         return result;
     }
 
+    @LoggerOperator(description = "删除缓存")
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public ActionResult<String> setKeyValue(String key) {
         ActionResult<String> result = new ActionResult<>();
