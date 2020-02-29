ae49f09822f8671b79eb8b6050aeea4aeb00f64e
xianggua <1013830051@qq.com>
2020-03-01 00:13:30 +0800

    [CHG] 表格改用bootstrap-table样式

--- src/main/resources/static/modules/system/js/log.js
+++ src/main/resources/static/modules/system/js/log.js
@@ -1,40 +1,95 @@
 $(function () {
-    layui.use(['table'], function () {
-        var table = layui.table;
-        table.render({
-            elem: '#logTable',
-            height: 480,
-            url: '/operatorLog/list',
-            request:{
-                pageName:'pageNo',
-                limitNames:'pageSize'
-            },
-            title: '操作日志表',
-            page: true,
-            cols: [[
-                {field: 'id', title: 'id', width: 80, fixed: 'left'},
-                {field: 'type', title: '类型', width: 80},
-                {field: 'title', title: '标题', width: 100},
-                {field: 'content', title: '内容', width: 80},
-                {field: 'method', title: '方法名称', width: 100},
-                {field: 'remoteAddr', title: '请求地址', width: 100},
-                {field: 'requestUri', title: 'URI', width: 120},
-                {field: 'params', title: '提交参数', width: 120},
-                {field: 'exception', title: '异常信息', width: 100},
-                {field: 'result', title: '执行结果', width: 200},
-                {field: 'createTime', title: '操作时间', width: 120},
-                {field: 'costTime', title: '执行耗时(ms)', width: 100},
-                {field: 'userName', title: '操作用户', width: 100}
-            ]],
-            limits:[10,20,50,100],
-            parseData: function (res) {
-                console.log(res)
-                return {
-                    "code": res.code,
-                    "data": res.data
-                }
-            }
-        })
-    })
+
+    // 初始化表格
+    initTable();
+
 
 });
+
+initTable = function () {
+    $("#logTable").bootstrapTable({
+        url: "/operatorLog/list",
+        method: 'GET',
+        // contentType:"application/x-www-form-urlencoded",
+        // dataType:"json",
+        // toolbar:'#toolbar',
+        striped: true,                      //是否显示行间隔色
+        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination: true,                   //是否显示分页（*）
+        sortable: true,                     //是否启用排序
+        sortOrder: "asc",                   //排序方式
+        // queryParams: queryParam,         //传递参数（*）
+        sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber: 1,                       //初始化加载第一页，默认第一页
+        pageSize: 10,                       //每页的记录行数（*）
+        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        // smartDisplay: false,
+        search: false,                      //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+        strictSearch: true,
+        showColumns: false,                 //是否显示所有的列
+        showRefresh: false,                 //是否显示刷新按钮
+        height: 480,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+        uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+        columns: [{
+            field: 'id',
+            title: 'id'
+        }, {
+            field: 'type',
+            title: '类型'
+        }, {
+            field: 'title',
+            title: '标题',
+            width: 100
+        }, {
+            field: 'content',
+            title: '内容',
+            width: 100
+        }, {
+            field: 'method',
+            title: '方法名称',
+            width: 120
+        }, {
+            field: 'remoteAddr',
+            title: '请求地址',
+            width: 120
+        }, {
+            field: 'requestUri',
+            title: 'URI',
+            width: 150
+        }, {
+            field: 'params',
+            title: '提交参数',
+            width: 150
+        }, {
+            field: 'exception',
+            title: '异常信息',
+            width: 120
+        }, {
+            field: 'result',
+            title: '执行结果'
+        }, {
+            field: 'createTime',
+            title: '操作时间',
+            formatter:function (res) {
+                var date = new Date(res);
+                return date.toLocaleString().replace(/\//g,'-');
+            }
+        }, {
+            field: 'costTime',
+            title: '执行耗时(ms)'
+        }, {
+            field: 'userName',
+            title: '操作用户'
+        }],
+        responseHandler: function (res) {
+            return res.data;
+        }
+    })
+}
+
+queryParam = function (params) {
+    return {
+        offset: params.offset,
+        limit: params.limit
+    }
+}
